2585131c9d1cc467757f91bdc8305e2b5c190597
shahirzain <shahirzain100@gmail.com>
2019-02-17 11:26:53 +0500

    final commit

--- src/component/Title.js
+++ src/component/Title.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Titles = () => (
-  <div className="App">
-    <h1>Weather Finder</h1>
-    <p>Find out temperature, conditions and more...</p>
+  <div>
+    <h1 className="title-container__title">Weather Finder</h1>
+    <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
   </div>
 )
 export default Titles;
--- src/component/form.js
+++ src/component/form.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Form = (props) => (
   <div>
-    <form onSubmit={props.getWeather}>
-      <input type="text" name="city" placeholder="city. . ." />
-      <input type="text" name="country" placeholder="country. . ." />
-      <input type="submit" value="Get Weather" />
-    </form>
+  <form onSubmit={props.getWeather}>
+		<input type="text" name="city" placeholder="City..."/>
+		<input type="text" name="country" placeholder="Country..."/>
+		<button>Get Weather</button>
+	</form>
   </div>
 )
 
